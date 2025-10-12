e387a152bd66fe79add5833970bf56e898092a0c
Aalphakeem-Adroit <alphakeem12@gmail.com>
2025-10-12 10:08:57 +0100

    Fix: User Model in Account Serializer

--- social_media_api/accounts/urls.py
+++ social_media_api/accounts/urls.py
@@ -4,5 +4,5 @@ from .views import RegisterView, LoginView, UserDetailView
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('me/', UserDetailView.as_view(), name='user-detail'),
+    path('profile/', UserDetailView.as_view(), name='user-detail'),
 ]
